25adca8be33d605934237286e44cdd301d69e857
DaniPraivet <133568745+DaniPraivet@users.noreply.github.com>
2024-11-05 20:58:10 +0100

    Added exercise uncomplete 5/11

---
+++ ejercicioClase7.java
@@ -0,0 +1,31 @@
+package Unidad5;
+
+import java.util.Arrays;
+
+public class ejercicioClase7 {
+    public static void main(String[] args) {
+        /*
+        Implementar la función:
+
+        int[] sinRepetidos(int t[])
+
+        Que construye y devuelve un array de longitud apropiada,
+        con los elementos de t, donde se han eliminado los datos
+        repetidos.*/
+
+        int[] t = {2,67,12,41,22,2,41};
+
+        for (int i = 0; i < t.length; i++) {
+
+
+        }
+
+
+
+
+
+    }
+    static int[] sinRepetidos(int[] t) {
+
+    }
+}
