9fc05b10d2d5b8f27195209f93d04e8c8c36592b
DaniPraivet <133568745+DaniPraivet@users.noreply.github.com>
2024-11-19 20:49:22 +0100

    New exercise undone

---
+++ Unidad6/ejercicioClase4.java
@@ -0,0 +1,32 @@
+package Unidad6;
+
+import java.util.Scanner;
+
+public class ejercicioClase4 {
+    public static void main(String[] args) {
+        /*Diseñar el juego “Acierta la contraseña”.
+
+        La mecánica del juego es la siguiente:
+
+        El primer jugador introduce la contraseña
+
+        El segundo jugador debe introducir palabras hasta que la acierte.
+
+        Realizar dos versiones:
+
+            Primera versión. El juego facilita al segundo jugador
+            información como por ejemplo si la contraseña es mayor o
+            menor que la palabra que está introduciendo
+
+            Segunda versión. El juego facilita al jugador información sobre
+            la longitud de la contraseña y una cadena de caracteres acertados
+            en sus lugares respectivos así como asteriscos en los no
+            acertados.*/
+
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Quieres jugar la primera version o la segunda?");
+
+    }
+}
