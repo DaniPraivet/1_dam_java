546d1937669879d7d95001c8daf029d231944347
rodriguez.pedan <rodriguez.pedan24@cordoba.salesianos.edu>
2025-04-22 17:43:39 +0200

    Class progress

--- Unidad13/Test.java
+++ Unidad13/Test.java
@@ -1,13 +1,17 @@
 package Unidad13;
 
 import java.sql.*;
+import java.util.Properties;
+import java.util.logging.Logger;
 
-public class Test {
+public class Test{
     public static void main(String[] args) {
         try {
-            Connection conexionDB = DriverManager.getConnection("jdbc:mysql://localhost:3306/test");
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conexionDB = DriverManager.getConnection("jdbc:mysql://172.26.12.94:3306/tienda","dani","usuario");
             Statement informeConsultaEmpleados = conexionDB.createStatement();
-            ResultSet conjuntoDeResultados = informeConsultaEmpleados.executeQuery("SELECT * FROM personas;");
+            ResultSet conjuntoDeResultados = informeConsultaEmpleados.executeQuery("SELECT * FROM producto;");
             while (conjuntoDeResultados.next()) {
                 System.out.println(conjuntoDeResultados);
             }
@@ -16,6 +20,8 @@ public class Test {
             conexionDB.close();
         } catch (SQLException e) {
             System.out.println(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
